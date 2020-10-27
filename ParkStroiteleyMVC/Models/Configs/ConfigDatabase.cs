b5c980e5c40f9a5b97d00f70c378c27b0422ec89
Kostya_dev <serviceafcstudio@yandex.ru>
2020-10-27 14:11:58 +0500

    ~rebuild Config DB

--- ParkStroiteleyMVC/Models/Configs/ConfigDatabase.cs
+++ ParkStroiteleyMVC/Models/Configs/ConfigDatabase.cs
@@ -7,6 +7,21 @@ namespace ParkStroiteleyMVC.Models.Configs
 {
     public static class ConfigDatabase
     {
-        public static readonly string ConnectionString = "Server=localhost;Database=ParkDB;User=User;Password=Lollipop_321123?;";
+        /// <summary>
+        /// DONT TOUCH IF DEBUG AND DEVELOP
+        /// </summary>
+        public static readonly string ConnectionString = $"Server=localhost;Database={DB};User={UserDB};Password={PasswordDB};";
+        /// <summary>
+        /// DONT TOUCH IF DEBUG AND DEVELOP
+        /// </summary>
+        public static readonly string UserDB = "UserPinguin";
+        /// <summary>
+        /// DONT TOUCH IF DEBUG AND DEVELOP
+        /// </summary>
+        public static readonly string PasswordDB = "KinderPingui321123";
+        /// <summary>
+        /// DONT TOUCH IF DEBUG AND DEVELOP
+        /// </summary>
+        public static readonly string DB = "ParkDB";
     }
 }
