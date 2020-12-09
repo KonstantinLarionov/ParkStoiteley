b8945876b07e74af8bedb2ef3c04db06e116d182
Kostya <kostya12277@yandex.ru>
2020-12-09 17:30:10 +0500

    loader and fix text wrapping mobile

--- ParkStroiteleyMVC/wwwroot/assets/js/main.js
+++ ParkStroiteleyMVC/wwwroot/assets/js/main.js
@@ -1,3 +1,28 @@
+
+$(window).on("load", function () {
+    setTimeout(function () { LoadOff() }, 2000);
+});
+
+function RootingAnimation(millisecond) {
+    LoadOn();
+    setTimeout(function () { LoadOff() }, millisecond);
+}
+function LoadOn() {
+    $("#right").css("transform", "translateX(0)");
+    $("#left").css("transform", "translateX(0)");
+    $(".item-door").css("box-shadow", "0 0px 0px 0px rgba(0,0,0,0), 0 0px 0px 0px rgba(0,0,0,0), 0 0px 0px 0 rgba(0,0,0,0)");
+    setTimeout(function () { $("#preloader").removeAttr("hidden") }, 1000);
+
+}
+function LoadOff() {
+    $("#preloader").attr("hidden", "");
+    $("#right").css("transform", "translateX(-100%)");
+    $("#left").css("transform", "translateX(100%)");
+    $(".item-door").css("0 8px 10px 1px rgba(0,0,0,0.14), 0 3px 14px 3px rgba(0,0,0,0.12), 0 4px 5px 0 rgba(0,0,0,0.20)");
+    setTimeout(function () {
+        $(".wrapper-slider").css("z-index", "0");
+    }, 1000);
+}
 $(document).ready(function () {
     $('#weusecookie').fadeIn(1000);
 
